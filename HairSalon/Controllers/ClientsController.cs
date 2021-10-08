2992bae03e6f5f38d2cd600be96c4a4df34a6ca1
Diego Serafico <dseraficohernandez@gmail.com>
2021-10-08 16:29:28 -0700

    add create form for client

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using HairSalon.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 
 namespace HairSalon.Controllers
 {
@@ -18,6 +20,20 @@ namespace HairSalon.Controllers
     public ActionResult Index()
     {
       return View();
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Details", "Stylists", new {id=client.StylistId});
     }
   }
 }
--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using HairSalon.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 
 namespace HairSalon.Controllers
 {
